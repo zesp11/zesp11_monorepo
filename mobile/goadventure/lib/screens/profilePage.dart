d0d072255595020c27ff95dddf199762a3c50dfb
kenny <kenny909@mat.umk.pl>
2024-12-14 11:39:44 +0100

    Add basic layout

---
+++ mobile/goadventure/lib/screens/profilePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Profile Screen (ProfilePage class)',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
