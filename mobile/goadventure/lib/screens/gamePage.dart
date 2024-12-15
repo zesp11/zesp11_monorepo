99aa509464642de0f1587629142fc2ba08583270
kenny <kenny909@mat.umk.pl>
2024-12-15 15:26:34 +0100

    Add layout to gamePage

--- mobile/goadventure/lib/screens/gamePage.dart
+++ mobile/goadventure/lib/screens/gamePage.dart
@@ -5,46 +5,40 @@ class GamePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Text("This is GamePage"),
+        const Expanded(
+          child: Center(
+            child: Text(
+              "This is GamePage class",
+              style: TextStyle(
+                fontSize: 32,
+              ),
+            ),
+          ),
+        ),
+        Flexible(
+          child: Column(
+            children:
+                ["Button 1", "Button 2", "Button 3", "Button 4"].map((label) {
+              return Expanded(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print('$label pressed');
+                    },
+                    child: Text(
+                      label,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
       ],
     );
   }
 }
-
-
-    // Column(
-    //   children: <Widget>[
-    //     const Expanded(
-    //       child: Center(
-    //         child: Text(
-    //           "Choose an action:",
-    //           textAlign: TextAlign.center,
-    //           style: TextStyle(
-    //             fontSize: 18,
-    //             fontWeight: FontWeight.bold,
-    //           ),
-    //         ),
-    //       ),
-    //     ),
-    //     Column(
-    //       children:
-    //           ["Button 1", "Button 2", "Button 3", "Button 4"].map((label) {
-    //         return SizedBox(
-    //           width: double.infinity, // fill horizontal space
-    //           child: ElevatedButton(
-    //               onPressed: () {
-    //                 print('$label pressed');
-    //               },
-    //               child: Text(
-    //                 label,
-    //                 style: const TextStyle(
-    //                   fontSize: 24,
-    //                 ),
-    //               )),
-    //         );
-    //       }).toList(),
-    //     )
-    //   ],
-    // ),
