1d7b0a1bf9607855677a072c3a4589bfd7f367ad
kenny <kenny909@mat.umk.pl>
2024-12-16 14:04:29 +0100

    Add loading indicator for game

---
+++ mobile/goadventure/lib/models/pokemon.dart
@@ -0,0 +1,19 @@
+class Pokemon {
+  final int id;
+  final String name;
+  final String imageUrl;
+
+  Pokemon({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['sprites']['front_default'],
+    );
+  }
+}
