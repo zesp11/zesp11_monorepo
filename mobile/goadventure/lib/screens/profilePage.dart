9b40ceda537ea7e8a3ba3fdca2131c1f50705b98
kenny <kenny909@mat.umk.pl>
2024-12-16 15:22:57 +0100

    Improve profile page

--- mobile/goadventure/lib/screens/profilePage.dart
+++ mobile/goadventure/lib/screens/profilePage.dart
@@ -5,10 +5,72 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Profile Screen (ProfilePage class)',
-        style: TextStyle(fontSize: 24),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.blueAccent,
+              child: Icon(
+                Icons.person,
+                size: 60,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "User name",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            // Placeholder for stats
+            const Text(
+              "Games Played: 0",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+
+            const SizedBox(
+              height: 5,
+            ),
+
+            const Text(
+              'Games finished: 0',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Edit Profile clicked!"),
+                  ),
+                );
+              },
+              child: const Text("Edit Profile"),
+            )
+          ],
+        ),
       ),
     );
   }
