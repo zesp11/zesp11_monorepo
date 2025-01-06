a8da9e6a3dfdcb2e4dbef17b7c31e60999198b2c
Kacper Urbanski <kacperurbanski002@gmail.com>
2025-01-07 00:02:14 +0100

    Handle login for multiple users

--- mobile/goadventure/lib/app/services/auth_service.dart
+++ mobile/goadventure/lib/app/services/auth_service.dart
@@ -1,17 +1,29 @@
+import 'package:get/get.dart';
 import 'package:goadventure/app/services/api_service/api_service.dart';
+import 'package:goadventure/main.dart';
+import 'package:logger/logger.dart';
 
 class AuthService {
   final ApiService apiService;
+  final logger = Get.find<Logger>();
 
   AuthService({required this.apiService});
   // Simulating local data for token
   static const String _mockToken = 'testToken';
 
   Future<String> login(String username, String password) async {
+    if (isProduction) {
+      logger.f("AuthService is not implemented in production");
+      UnimplementedError();
+    }
+
+    // INFO: this is only for development GET RID OF IF IN PRODUCTION
     // Simulate checking the username and password locally
-    if (username == 'testUsername' && password == 'testPassword') {
+    if ((username == '1' && password == '1') ||
+        (username == '2' && password == '2') ||
+        (username == '3' && password == '3')) {
       // Simulating a successful login by returning the mock token
-      return _mockToken;
+      return username; // return he's login
     } else {
       // Simulate a login failure
       throw Exception('Invalid username or password');
