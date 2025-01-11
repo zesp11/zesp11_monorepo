fdcbd3ac1cc0021fda75bccbffd32173f6db3fdf
Kacper Urbanski <kacperurbanski002@gmail.com>
2025-01-11 12:21:05 +0100

    Clean up api methods

--- mobile/goadventure/lib/app/bindings/home_binding.dart
+++ mobile/goadventure/lib/app/bindings/home_binding.dart
@@ -1,14 +1,13 @@
 import 'package:get/get.dart';
 import 'package:goadventure/app/controllers/home_controller.dart';
 import 'package:goadventure/app/services/api_service/api_service.dart';
-import 'package:goadventure/app/services/api_service/developmentApiService.dart';
 import 'package:goadventure/app/services/home_service.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
     // Inject HomeService
-    Get.put<ApiService>(DevelopmentApiService());
+    Get.find<ApiService>();
     Get.put<HomeService>(HomeService(apiService: Get.find()));
     Get.put<HomeController>(HomeController(homeService: Get.find()));
   }
--- mobile/goadventure/lib/app/bindings/profile_binding.dart
+++ mobile/goadventure/lib/app/bindings/profile_binding.dart
@@ -1,13 +1,12 @@
 import 'package:get/get.dart';
 import 'package:goadventure/app/controllers/profile_controller.dart';
 import 'package:goadventure/app/services/api_service/api_service.dart';
-import 'package:goadventure/app/services/api_service/developmentApiService.dart';
 
 class ProfileBinding extends Bindings {
   @override
   void dependencies() {
     // Inject ApiService and UserProfileController
-    Get.put<ApiService>(DevelopmentApiService());
+    Get.find<ApiService>();
     Get.put<ProfileController>(ProfileController(userService: Get.find()));
   }
 }
