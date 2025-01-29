4d8838538ee51e4aa683d156d60dc56e418e6829
Kacper Urbanski <kacperurbanski002@gmail.com>
2025-01-29 22:33:13 +0100

    Remotely fetch user data

--- mobile/goadventure/lib/app/controllers/search_controller.dart
+++ mobile/goadventure/lib/app/controllers/search_controller.dart
@@ -4,47 +4,6 @@ import 'package:get/get.dart';
 import 'package:goadventure/app/services/search_service.dart';
 import 'package:goadventure/main.dart';
 
-// class SearchController extends GetxController {
-//   final ApiService apiService = Get.find();
-
-//   // Observables
-//   var searchResults = [].obs;
-
-//   void search(String query, String category) async {
-//     // searchResults.value = await apiService.search(query, category);
-//   }
-// }
-
-// class SearchController extends GetxController {
-//   final SearchService searchService; // Declare the SearchService dependency
-
-//   // Reactive query for search
-//   var query = ''.obs;
-
-//   // TODO: add types instead of Map<String, String>
-//   // List of filtered items based on the query
-//   Rx<List<Map<String, String>>> filteredItems =
-//       Rx<List<Map<String, String>>>([]);
-
-//   // Constructor accepting SearchService
-//   SearchController({required this.searchService});
-
-//   // Update the query value
-//   void updateQuery(String value) {
-//     query.value = value;
-//     searchItems(value); // Trigger search when query changes
-//   }
-
-//   // Perform the search by calling the SearchService
-//   Future<void> searchItems(String query) async {
-//     // Optionally: You can also pass a category as an argument
-//     // TODO: make the categories enum
-//     var results = await searchService.search(
-//         query, 'all'); // Example: Search across all categories
-//     filteredItems.value = results;
-//   }
-// }
-
 class SearchController extends GetxController
     with StateMixin<List<Map<String, String>>> {
   final SearchService searchService;
@@ -75,11 +34,12 @@ class SearchController extends GetxController
 
       // If the query is empty, fetch all items
       if (query.isEmpty) {
-        results = await searchService.search('', 'all'); // Fetch all items
+        // FIXME: switch to categories instead of hard coded user
+        results = await searchService.search('', 'user'); // Fetch all items
         allItems.value = results;
       } else {
         results = await searchService.search(
-            query, 'all'); // Filter based on the query
+            query, 'user'); // Filter based on the query
       }
 
       if (!isProduction && query == 'error') {
